d0031fb495e19baf3827ce9e3e6eaa25659cc85a
VasiliyErmolaev <vasiliy.ermolaev@iplast.com>
2022-09-13 16:55:46 +0300

    Добавлена автоматическая установка зависимостей

--- run.py
+++ run.py
@@ -1,9 +1,39 @@
-from iMES import app,socketio,host,port
-from iMES.Controller.ShiftTaskDaemon import ShiftTaskDaemon
+import importlib,os
 
 
+installed = False
+def Dependency_check():
+    dependences = ['flask',
+                   'flask_login',
+                   'pyodbc',
+                   'progress',
+                   'flask_socketio']
+    not_install = []
+    for dependency in dependences:
+        try:
+            check_module = importlib.import_module(dependency)
+        except ModuleNotFoundError:
+            not_install.append(dependency)
+    if(len(not_install) > 0):
+        install_str = "pip install"
+        for module in not_install:
+            install_str = f"{install_str} {module}"
+        try:
+            os.system(install_str)
+        except:
+            pass
+Dependency_check()
+while installed == False:
+    print(installed)
+    try:    
+        import importlib
+        from iMES import app,socketio,host,port
+        from iMES.Controller.ShiftTaskDaemon import ShiftTaskDaemon
+        installed = True
 
-
-if __name__ == "__main__":
-    ShiftTaskMonitoring = ShiftTaskDaemon().Start()
-    socketio.run(app,host=host,port=port,debug=True)
+        if __name__ == "__main__":
+            ShiftTaskMonitoring = ShiftTaskDaemon().Start()
+            socketio.run(app,host=host,port=port,debug=True)
+            print(installed)
+    except ModuleNotFoundError:
+        pass
