ef92b22f02f9897cb7546bc50d5aa2a13ffeef6a
MaratTazeev <marat.tazeev@iplast.com>
2022-10-13 13:34:35 +0300

    Äîáàâèë âûáîð ïðîäóêòà ïðè ââîäà âåñà Ïî÷èíèë ñêðîëë ïðè âûáîðå îòõîäîâ

--- run.py
+++ run.py
@@ -39,4 +39,4 @@ if __name__ == "__main__":
     ShiftTaskMonitoring.Start()             
     ProductDataMonitoring = ProductionDataDaemon()
     ProductDataMonitoring.Start()
-    socketio.run(app, host=host, port=port)
+    socketio.run(app, host=host, port=port, debug=True)
