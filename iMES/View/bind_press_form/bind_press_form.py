04ea26b030b5f2bf57e2fd74c8adf98728ecdc51
RuslanAtakishiev <ruslan.atakishiev@iplast.com>
2022-11-28 11:37:01 +0300

    Исправлена привязка пресс-форм

--- iMES/View/bind_press_form/bind_press_form.py
+++ iMES/View/bind_press_form/bind_press_form.py
@@ -4,6 +4,7 @@ from flask import render_template, request
 from iMES import current_tpa,TpaList
 from iMES.Model.SQLManipulator import SQLManipulator
 from flask_login import login_required
+import datetime
 
 
 @login_required
@@ -24,17 +25,22 @@ def bindPressForms():
 
 
 @socketio.on('press_form_binding')
-def handle_selected_press_forms(json):
-    selected_press_form = list(json)
+def handle_selected_press_forms(data):
+    ip_addr = request.remote_addr
+    selected_press_form = str(data)
 
-    # Вытаскиваем Oid метки из последнего смыкания контроллера
-    sql_GetLabelOid = f"""SELECT TOP (1) [Label]
-                                        FROM [MES_Iplast].[dbo].[RFIDClosureData] 
-                                        WHERE Controller='{selected_press_form[1]}' 
-                                        ORDER BY GETDATE() DESC"""
-    label = SQLManipulator.SQLExecute(sql_GetLabelOid)
+    if current_tpa[ip_addr][2].controller != 'Empty':
+        # Вытаскиваем Oid метки из последнего смыкания контроллера
+        sql_GetLabelOid = f"""SELECT TOP (1) [Label]
+                                            FROM [MES_Iplast].[dbo].[RFIDClosureData] 
+                                            WHERE Controller='{current_tpa[ip_addr][2].controller}' 
+                                            ORDER BY Date DESC"""
+        label = SQLManipulator.SQLExecute(sql_GetLabelOid)
 
-    # Ищем старую запись по Oid метке из последнего смыкания и перезаписываем значение на Oid новой метки
-    SQLManipulator.SQLExecute(f"""UPDATE RFIDEquipmentBinding
-                                    SET Equipment = '{selected_press_form[0]}'
-                                    WHERE RFIDEquipment = '{label[0][0]}'""")
+        # Ищем старую запись по Oid метки из последнего смыкания и перезаписываем значение на Oid новой метки
+        SQLManipulator.SQLExecute(f"""UPDATE RFIDEquipmentBinding
+                                        SET Equipment = '{selected_press_form}'
+                                        WHERE RFIDEquipment = '{label[0][0]}'""")
+    
+    else:
+        app.logger.critical(f"[{datetime.datetime.now()}] Нет привязки контроллера к ТПА ({current_tpa[ip_addr][2].tpa})")
