51c2cbdc0019fec5d8455218bf730468df0409cc
VasiliyErmolaev <vasiliy.ermolaev@iplast.com>
2023-02-02 11:36:46 +0300

    Исправление задержки сервера

--- run.py
+++ run.py
@@ -1,38 +1,8 @@
 import importlib
 import os
 
-# Функция проверки установленных библиотек
-def Dependency_check():
-    dependences = ['flask',
-                   'flask_login',
-                   'pyodbc',
-                   'progress',
-                   'flask_socketio',
-                   'requests',
-                   'bs4']
-    not_install = []
-    for dependency in dependences:
-        try:
-            check_module = importlib.import_module(dependency)
-        except ModuleNotFoundError:
-            not_install.append(dependency)
-    if(len(not_install) > 0):
-        install_str = "pip install"
-        for module in not_install:
-            install_str = f"{install_str} {module}"
-        try:
-            os.system(install_str)
-        except:
-            pass
-
-# Пытаемся импортировать модули, если не получилось, загружаем через pip
-while True:
-    try:
-        from iMES import app, socketio, host, port
-        break
-    except ModuleNotFoundError:
-        Dependency_check()
+from iMES import app, socketio, host, port
 
 # Точка входа в основной цикл программы
 if __name__ == "__main__":
-    socketio.run(app, host=host, port=port, debug=True, use_reloader=True)
+    socketio.run(app, host=host, port=port)
