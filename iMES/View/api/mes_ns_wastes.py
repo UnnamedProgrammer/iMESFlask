e129ba167dfe744525da2003f7cadb7a04b60fdc
VasiliyErmolaev <vasiliy.ermolaev@iplast.com>
2023-04-04 08:19:35 +0300

    Исправление ошибки API по отходам

--- iMES/View/api/mes_ns_wastes.py
+++ iMES/View/api/mes_ns_wastes.py
@@ -16,29 +16,30 @@ def mes_ns_wastes():
         if str(tpa[0]) == tpa_oid:
             selected_tpa = tpa
             break
-    if selected_tpa is not None:
-        production_data = db.session.query(ProductionData).where(
-            ProductionData.ShiftTask == selected_tpa[2].shift_task_oid[0]).first()
-        wastes_list = db.session.query(ProductWaste).where(
-            ProductWaste.ProductionData == production_data.Oid
-        ).all()
-        for waste in wastes_list:
-            material = None
-            waste_type = 'Брак'
-            if waste.Material is not None:
-                material = db.session.query(Material).where(Material.Oid == waste.Material).first()
-            if waste.Type == 0:
-                waste_type = 'Отход'
-            usr = db.session.query(User).where(User.Oid == waste.Creator).one_or_none()
-            creator_name = ""
-            if creator_name is not None:
-                creator_name = usr.get_name()
-            wastes_json[str(waste.Oid)] = {
-                'type': str(waste_type),
-                'material': material.Name if material is not None else '',
-                'weight': str(waste.Weight),
-                'count': str(waste.Count),
-                'create_date': str(waste.CreateDate),
-                'creator': creator_name
-            }
+    if (selected_tpa is not None):
+        if selected_tpa[2].shift_task_oid[0] != '':
+            production_data = db.session.query(ProductionData).where(
+                ProductionData.ShiftTask == selected_tpa[2].shift_task_oid[0]).first()
+            wastes_list = db.session.query(ProductWaste).where(
+                ProductWaste.ProductionData == production_data.Oid
+            ).all()
+            for waste in wastes_list:
+                material = None
+                waste_type = 'Брак'
+                if waste.Material is not None:
+                    material = db.session.query(Material).where(Material.Oid == waste.Material).first()
+                if waste.Type == 0:
+                    waste_type = 'Отход'
+                usr = db.session.query(User).where(User.Oid == waste.Creator).one_or_none()
+                creator_name = ""
+                if creator_name is not None:
+                    creator_name = usr.get_name()
+                wastes_json[str(waste.Oid)] = {
+                    'type': str(waste_type),
+                    'material': material.Name if material is not None else '',
+                    'weight': str(waste.Weight),
+                    'count': str(waste.Count),
+                    'create_date': str(waste.CreateDate),
+                    'creator': creator_name
+                }
     return wastes_json
